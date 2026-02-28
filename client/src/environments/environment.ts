8467143b63ff79d02b18627795f3582ce6db828f
sai-0309 <22pa1a0309@vishnu.edu.in>
2026-02-28 19:34:16 +0530

    add

--- client/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    apiUrl: 'https://shopsmart-api-eid2.onrender.com/api', // Use YOUR actual Render URL here
-    stripePublicKey: 'pk_test_placeholder',
-};
